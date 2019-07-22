6cbd226cd07e82665f28b4c6f335b0309ebb939f
NicoleVenachi <ludwing@unicauca.edu.co>
2019-07-22 07:37:45 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,5 @@
+$(function() {
+	//funcion que se ejectua cuando se termina de jecutar tod el sitio
+	$('[data-toggle="tooltip"]').tooltip() //tomo elementos con atributo ata-toggle igual a tooltip 
+					//llamo funcion tooltip sobre ellos
+})
